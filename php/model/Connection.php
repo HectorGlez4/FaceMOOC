0d790d82ab15586edf301787c6c446791ee6b5cd
Linda Gutierrez <lindaflor_gutierrez@hotmail.com>
2016-03-16 16:17:40 +0100

    ajout des donness

--- php/model/Connection.php
+++ php/model/Connection.php
@@ -3,7 +3,7 @@
 	{
 		public $Server="localhost";
 		public $User="root";
-		public $Password="root";
+		public $Password="";
 		//public $Password="123456";
 		//public $Password="";
 		public $BD="mooc";

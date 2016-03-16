a9b511dc2a7b89e996f825f71b2f96fde5d70a80
PruDyl <dylan.prudhomme@gmail.com>
2016-03-16 10:32:38 +0100

    affichage formation beta

--- php/view/home.php
+++ php/view/home.php
@@ -1,30 +1,36 @@
 <?php
-include "../model/MFormation.php";
-
 $mformation = new MFormation();
-$mformation->SelectFormations(1);
-var_dump($mformation);
+$formations = $mformation->SelectFormationsPage(1);
+var_dump($formations);
 ?>
 <h1>Bienvenue sur le site !</h1>
 
 <body>
 	<?php
-		/*for(i=1; i<=count($mformation['id']); i++){
+		for($i = 0; $i <= count($formations) -1; $i++){
+			$formationsId = $formations[$i]['id_formation'];
+			var_dump($formationsId);
+			$formationsTitle = $formations[$i]['title'];
+			$formationsImage = $formations[$i]['image'];
+			$formationsReqSkills = $formations[$i]['required_skill'];
+			$formationsDifficulty = $formations[$i]['difficulty'];
 			echo "<div class ='row'>";
-			for(j=1; j<=4; j++){
+			for($j=1; $j<=4; $j++){
 				echo "<a href='#'>";
 				echo "<div class ='column-md-3'>";
-				echo "<h2>$classTitle</h2>";
-				echo "<p>$classChapters Chapter</p>";
+				echo "<h2>$formationsTitle</h2>";
+				echo "<img src='$formationsImage' alt='Image formation'>";
+				echo "<p>Difficulty : $formationsDifficulty </p>";
+				echo "<p>Required skills : $formationsReqSkills </p>";
 				echo "<div>";
 				echo "</a>";
-				i++;
-				if(i > count($class)){
+				$i++;
+				if($i > count($formations) - 1){
 					break;
 				}
 			}
 			echo "</div>";
-		}*/
+		}
 	?>
 </body>
 <a href='<?php WEBROOT ?>User/logout'>Deconnexion</a>

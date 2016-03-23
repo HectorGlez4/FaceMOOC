56d2f3b8fb1c859cc875e05d4fd7e021f35e35c4
Marvyn <marvyn.duvauchelle@gmail.com>
2016-03-23 14:45:39 +0100

    qqu modifs

--- php/view/editFormationContent.php
+++ php/view/editFormationContent.php
@@ -50,7 +50,7 @@ global $content;
                 COURS
                 <div class="form-group">
                     <div class="input-group">
-                        <input type="file" name="videoClass"/>
+                        <input type="file" name="cours"/>
                     </div>
                 </div>
                 <div class="row">

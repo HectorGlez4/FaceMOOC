b27bc84ea566760c31c934fae4a127789f587e11
PruDyl <dylan.prudhomme13@gmail.com>
2016-03-24 15:20:54 +0100

    change controller.php en Controller.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 define('WEBROOT', str_replace('index.php','',$_SERVER['SCRIPT_NAME']));
 define('ROOT', str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
-require(ROOT.'php/controller/controller.php');
+require(ROOT.'php/controller/Controller.php');
 $params = explode('/', $_GET['p']);
 $controller = $params[0];
 $action = isset($params[1]) ? $params[1] : 'index';

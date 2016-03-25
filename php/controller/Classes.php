5bd3093de59b2f15c329cdaa5031c6e0cbc4a6e2
PruDyl <dylan.prudhomme13@gmail.com>
2016-03-25 08:18:14 +0100

    ajout de commentaires

--- php/controller/Classes.php
+++ php/controller/Classes.php
@@ -1,28 +1,50 @@
 <?php
+/**
+ * Controlleur des classes
+ * Envoie les donner à afficher à la vue class.php
+ *
+**/
 
 include_once(ROOT.'php/model/MFormation.php');
 include_once(ROOT.'php/model/MChapter.php');
 include_once(ROOT.'php/model/MClass.php');
 include_once(ROOT.'php/model/MUser.php');
 
-
+/**
+ * Class du controlleur des classes contenu dans les chapitres
+ *
+**/
 Class Classes extends Controller {
 	var $layout;
 
+    /**
+     * Fonction gérant l'affichage sur la page d'accueil du controlleur
+     *
+     * @param int $id
+     *      ID de la personne connectée
+    **/
     function index($id) {
+        // Si l'utilisateur n'est pas connecté, 
+        //il est redirigé vers la page de connexion
         if(!isset($_SESSION['id'])) {
             header('Location:'.WEBROOT.'User');
             return;
         }
+        // Création des objets
         $MClass = new MClass();
-         $MUser = new MUser();
+        $MUser = new MUser();
+        // Utilisation des méthodes des objets
+        // les données retourner par les méthodes sont stockées dans le tableau $d
         $d['userInfo'] = $MUser->SelectUserEmail($_SESSION['email']);
         $d['formation'] = $MClass->SelectInfoByJoin('f.*', $id);
         $d['chapter'] = $MClass->SelectInfoByJoin('ch.*', $id);
         $d['currentclass'] = $MClass->SelectClass($id);
+        // Si la classe courrante n'existe pas, on affiche la page d'erreur 404
         if ($d['currentclass'] == null) {
             require(ROOT.'php/view/error.php');
         }
+        // Sinon les chapitres de la class courrante, 
+        //ainsi qu'elle même sont envoyés à la vue
         else{
             $chapters = $d['chapter'];
             //var_dump($chapters);
@@ -31,6 +53,8 @@ Class Classes extends Controller {
                 $d['class'][$chapter['id_chapter']] = $MClass->SelectClassByChapterId($chapter['id_chapter']);
                 //var_dump($d['class']);
             }
+            // Toutes les données stockées dans le tablea $d
+            // sont envoyées vers la vu class.php
             $this->set($d);
             $this->render('class');
         }
--- php/controller/Formation.php
+++ php/controller/Formation.php
@@ -1,14 +1,29 @@
 <?php
 
+/**
+ * Controlleur des formations
+ *
+**/
+
 include_once(ROOT.'php/model/MFormation.php');
 include_once(ROOT.'php/model/MChapter.php');
 include_once(ROOT.'php/model/MClass.php');
 
+/**
+ * Class du controlleur des formations 
+ *
+**/
 Class Formation extends Controller {
 
 	var $layout;
 
+    /**
+     * Fonction gérant l'affichage sur la page d'accueil du controlleur
+     *
+    **/
     function index() {
+        // Si l'utilisateur n'est pas connecté, 
+        //il est redirigé vers la page de connexion
         if(!isset($_SESSION['id'])) {
             header('Location:'.WEBROOT.'User');
             return;
@@ -16,18 +31,34 @@ Class Formation extends Controller {
         $this->render('formation');
     }
 
+    /**
+     * Fonction gérant l'affichage en fonction de la formation sélectionnée
+     *
+     * @param int $id
+     *      ID de la formation sélectionnée
+    **/
     function view($id){
+        // Si l'utilisateur n'est pas connecté, 
+        //il est redirigé vers la page de connexion
     	if(!isset($_SESSION['id'])) {
             header('Location:'.WEBROOT.'User');
             return;
         }
+        // Création des objets
         $MFormation = new MFormation();
         $MChapter = new MChapter();
         $MClass = new MClass();
+        // Utilisation des méthodes des objets
+        // les données retourner par les méthodes sont stockées dans le tableau $d
         $d['formation'] = $MFormation->SelectFormationById($id);
+        // Si la formation sélectionnée n'existe pas,
+        // on affiche la page d'erreur 404
         if ($d['formation'] == null) {
             echo require(ROOT.'php/view/error.php');
-        }else{
+        }
+        // Sinon on affiche les chapitres et les classes des chapitres
+        // de la formation concernée
+        else{
             $d['chapter'] = $MChapter->SelectChapters($id);
             $chapters = $d['chapter'];
             //var_dump($chapters);
@@ -38,6 +69,8 @@ Class Formation extends Controller {
                    // var_dump($d['class']);
                 }
             }
+            // Toutes les données stockées dans le tablea $d
+            // sont envoyées vers la vu formation.php
             $this->set($d);
             $this->render('formation');
             return;
